5cfae644cac76046499ad7492c9fce72c21d0348
Azevedo Rayldo <azevedorayldo@gmail.com>
2022-04-25 21:42:46 -0300

    primeiro commit

---
+++ src/main/java/br/com/azevedo/rayldo/expo_veiculos/ExpoVeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.azevedo.rayldo.expo_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpoVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpoVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/azevedo/rayldo/expo_veiculos/ExpoVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.azevedo.rayldo.expo_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpoVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
